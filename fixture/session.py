b651fbe152b35a99e92895c7864a0465d3469e63
AllexBykov <lexsbykov@gmail.com>
2023-02-14 20:05:21 +0600

    Отдельно вывел класс SessionHelper

---
+++ fixture/session.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.common.by import By
+
+
+class SessionHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def login(self, username, password):
+        wd = self.app.driver
+        self.app.open_home_page()
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys(username)
+        wd.find_element(By.NAME, "pass").click()
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys(password)
+        wd.find_element(By.ID, "LoginForm").click()
+        wd.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
+
+    def logout(self):
+        wd = self.app.driver
+        wd.find_element(By.LINK_TEXT, "Logout").click()
